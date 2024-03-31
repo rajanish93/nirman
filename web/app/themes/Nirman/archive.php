cfb7d5e6ecbfde9d7e78848c438cfd12fb717531
rajanish <singhrajanish31@gmail.com>
2024-03-31 12:11:29 +0530

    changes

--- web/app/themes/Nirman/archive.php
+++ web/app/themes/Nirman/archive.php
@@ -7,44 +7,153 @@
  * @package education
  */
 
-get_header();
-?>
+get_header();?>
+<?php if ( have_posts() ) : ?>
+	<!-- Header Start -->
+	<div class="container-fluid page-header">
+		<div class="container">
+			<div class="d-flex flex-column justify-content-center" style="min-height: 300px">
+				<h3 class="display-4 text-white text-uppercase">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+				</h3>
 
-	<main id="primary" class="site-main">
+			</div>
+		</div>
+	</div>
+	<!-- Header End -->
+	<?php endif; ?>
 
-		<?php if ( have_posts() ) : ?>
+	<!-- Detail Start -->
+	<div class="container-fluid py-5">
+		<div class="container py-5">
+			<div class="row">
+				<div class="col-lg-8">
+					<div class="mb-3">
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+						<main id="primary" class="site-main">
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+							<?php if ( have_posts() ) : ?>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
+								<?php
+								/* Start the Loop */
+								while ( have_posts() ) :
+									the_post();
 
-			the_posts_navigation();
+									get_template_part( 'template-parts/post-content', get_post_type() );
 
-		else :
+								endwhile;
 
-			get_template_part( 'template-parts/content', 'none' );
+								the_posts_navigation();
 
-		endif;
-		?>
+							else :
 
-	</main><!-- #main -->
+								get_template_part( 'template-parts/content', 'none' );
+
+							endif;
+							?>
+
+						</main><!-- #main -->
+					</div>
+
+				</div>
+
+				<div class="col-lg-4 mt-5 mt-lg-0">
+
+					<!-- Category List -->
+					<div class="mb-5">
+						<h3 class="text-uppercase mb-4">Some Important Links</h3>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+								<a href="" class="text-decoration-none h6 m-0">Prelims Test Series 2022</a>
+								<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+										aria-hidden="true"></i></span>
+							</li>
+							<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+								<a href="" class="text-decoration-none h6 m-0">Distance Learning Program</a>
+								<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+										aria-hidden="true"></i></span>
+							</li>
+							<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+								<a href="" class="text-decoration-none h6 m-0">PCS</a>
+								<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+										aria-hidden="true"></i></span>
+							</li>
+							<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+								<a href="" class="text-decoration-none h6 m-0">Practice Quiz</a>
+								<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+										aria-hidden="true"></i></span>
+							</li>
+							<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+								<a href="" class="text-decoration-none h6 m-0">Daily Current Affairs and Editorials</a>
+								<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+										aria-hidden="true"></i></span>
+							</li>
+						</ul>
+					</div>
+
+					<!-- Dates -->
+					<div class="mb-5">
+
+
+					</div>
+
+
+
+					<!-- Recent Post -->
+					<div class="mb-5 mt-4">
+						<h3 class="text-uppercase mb-4 mt-5">Recent Post</h3>
+
+
+						<?php $recent_posts = wp_get_recent_posts(array(
+                            'numberposts' => 3, 
+                            'post_type'=> 'post',
+                            'post_status' => 'publish', 
+                            'orderby'          => 'post_date',
+                            'order'            => 'DESC',
+                            //'posts_per_page' => -1,
+                            
+                        ));
+                 //  print_r($recent_posts); die;
+                    foreach($recent_posts as $post) : ?>
+						<?php
+                            
+                            if(get_the_post_thumbnail($post['ID'], 'full') ){
+                    
+                                $articalThubUrl=get_the_post_thumbnail_url($post['ID'], 'thumbnail');
+                            }else {
+                                $articalThubUrl="/artical.png";
+                            }
+
+                            $permalink=get_permalink( $post['ID'] );
+
+                            $postDaate=date("M d, Y",strtotime($post['post_date']));
+                        ?>
+
+
+
+						<a class="d-flex align-items-center text-decoration-none mb-3" href="<?php echo $permalink;?>">
+
+							<div class="pl-0">
+								<h6 class="m-1"><?php echo $post['post_title'] ?>"</h6>
+								<small><?php echo $postDaate;?></small>
+							</div>
+						</a>
+
+						<?php endforeach; wp_reset_query(); ?>
+
+
+					</div>
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Detail End -->
 
 <?php
 get_sidebar();
--- web/app/themes/Nirman/single.php
+++ web/app/themes/Nirman/single.php
@@ -1,40 +1,149 @@
 <?php
 /**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package education
- */
+ * Template Name: Syllabus
 
-get_header();
+ */
+$pid = get_the_id();
+$featured_img_url = get_the_post_thumbnail_url($post->ID, 'full');
 ?>
 
-	<main id="primary" class="site-main">
+<?php get_header(); ?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+<!-- Header Start -->
+<div class="container-fluid page-header">
+	<div class="container">
+		<div class="d-flex flex-column justify-content-center" style="min-height: 300px">
+			<h3 class="display-4 text-white text-uppercase">Blog </h3>
+			
+		</div>
+	</div>
+</div>
+<!-- Header End -->
 
-			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'education' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'education' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+<!-- Detail Start -->
+<div class="container-fluid py-5">
+	<div class="container py-5">
+		<div class="row">
+			<div class="col-lg-8">
+				<div class="mb-3">
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+					<?php
+						while ( have_posts() ) :
+							the_post();
 
-		endwhile; // End of the loop.
-		?>
+							get_template_part( 'template-parts/post-content', get_post_type() );
 
-	</main><!-- #main -->
+							// the_post_navigation(
+							// 	array(
+							// 		'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'education' ) . '</span> <span class="nav-title">%title</span>',
+							// 		'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'education' ) . '</span> <span class="nav-title">%title</span>',
+							// 	)
+							// );
 
-<?php
-get_sidebar();
-get_footer();
+							// If comments are open or we have at least one comment, load up the comment template.
+							// if ( comments_open() || get_comments_number() ) :
+							// 	comments_template();
+							// endif;
+
+						endwhile; // End of the loop.
+						?>
+				</div>
+
+			</div>
+
+			<div class="col-lg-4 mt-5 mt-lg-0">
+
+				<!-- Category List -->
+				<div class="mb-5">
+					<h3 class="text-uppercase mb-4">Some Important Links</h3>
+					<ul class="list-group list-group-flush">
+						<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+							<a href="" class="text-decoration-none h6 m-0">Prelims Test Series 2022</a>
+							<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+									aria-hidden="true"></i></span>
+						</li>
+						<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+							<a href="" class="text-decoration-none h6 m-0">Distance Learning Program</a>
+							<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+									aria-hidden="true"></i></span>
+						</li>
+						<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+							<a href="" class="text-decoration-none h6 m-0">PCS</a>
+							<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+									aria-hidden="true"></i></span>
+						</li>
+						<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+							<a href="" class="text-decoration-none h6 m-0">Practice Quiz</a>
+							<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+									aria-hidden="true"></i></span>
+						</li>
+						<li class="list-group-item d-flex justify-content-between align-items-center px-0">
+							<a href="" class="text-decoration-none h6 m-0">Daily Current Affairs and Editorials</a>
+							<span class="badge badge-primary badge-pill"><i class="fa fa-angle-double-right"
+									aria-hidden="true"></i></span>
+						</li>
+					</ul>
+				</div>
+
+				<!-- Dates -->
+				<div class="mb-5">
+
+
+				</div>
+
+
+
+				<!-- Recent Post -->
+				<div class="mb-5 mt-4">
+					<h3 class="text-uppercase mb-4 mt-5">Recent Post</h3>
+
+
+					<?php $recent_posts = wp_get_recent_posts(array(
+                            'numberposts' => 3, 
+                            'post_type'=> 'post',
+                            'post_status' => 'publish', 
+                            'orderby'          => 'post_date',
+                            'order'            => 'DESC',
+                            //'posts_per_page' => -1,
+                            
+                        ));
+                 //  print_r($recent_posts); die;
+                    foreach($recent_posts as $post) : ?>
+					<?php
+                            
+                            if(get_the_post_thumbnail($post['ID'], 'full') ){
+                    
+                                $articalThubUrl=get_the_post_thumbnail_url($post['ID'], 'thumbnail');
+                            }else {
+                                $articalThubUrl="/artical.png";
+                            }
+
+                            $permalink=get_permalink( $post['ID'] );
+
+                            $postDaate=date("M d, Y",strtotime($post['post_date']));
+                        ?>
+
+
+
+					<a class="d-flex align-items-center text-decoration-none mb-3" href="<?php echo $permalink;?>">
+
+						<div class="pl-0">
+							<h6 class="m-1"><?php echo $post['post_title'] ?>"</h6>
+							<small><?php echo $postDaate;?></small>
+						</div>
+					</a>
+
+					<?php endforeach; wp_reset_query(); ?>
+
+
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Detail End -->
+
+<?php get_footer(); ?>
